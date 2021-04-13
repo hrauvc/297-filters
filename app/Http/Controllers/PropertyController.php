1872d6d06bb61b6518e8384c820ccfc4ed5874ea
Homero Raul <h.raulvc@gmail.com>
2021-04-12 21:13:12 -0500

    Update PropertyController.php

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -11,19 +11,35 @@ class PropertyController extends Controller
 	public function filter(Request $request, Property $property)
 	{
 
+
+		$query = $property->newQuery();
+
+
 		if($request->id){
 
-			$property = $property->where('id', $request->id);
+			$query->where('id', $request->id);
 
 		}	
 
 		if($request->name){
 
-			$property = $property->where('name', $request->name);
+			$query->where('name', 'like', '%' . $request->name . '%');
 
 		}	
 
-		return response()->json($property->get());
+		if($request->features) {
+
+			$query->whereHas('features', function($q) use ($request) {
+
+				$q->whereIn('features.id', $request->features);
+
+			});
+
+		}
+
+
+
+		return response()->json($query->get());
 
 	}
 
